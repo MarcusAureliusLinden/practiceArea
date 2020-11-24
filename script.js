4cecd2171960a78006bc20cff32a1855bdd9f49e
MarcusAureliusLinden <gottemaja@gmail.com>
2020-11-24 03:41:57 +0000

    aaaa

--- script.js
+++ script.js
@@ -1,50 +1,61 @@
-const baseURL = "https://ci-swapi.herokuapp.com/api/";
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+const countdownBoard = document.querySelector(".countdown");
+const startButton = document.querySelector(".startButton");
 
-function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
+let lastHole;
+let timeUp = false;
+let timeLimit = 20000;
+let score = 0;
+let countdown; 
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
+function pickRandomHole(holes) {
+    const randomHole = Math.floor(Math.random() * holes.length);
+    const hole = holes[randomHole];
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
+    return hole;
 }
 
-function getTableHeaders(obj) {
-    var tableHeaders = [];
-
-    Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td>${key}</td>`)
-    });
-
-    return `<tr>${tableHeaders}</tr>`;
-}
-
-function writeToDocument(type) {
-    var tableRows = [];
-    var el = document.getElementById("data");
-    el.innerHTML = "";
-
-    getData(type, function(data) {
-        data = data.results;
-        var tableHeaders = getTableHeaders(data[0]);
-
-        data.forEach(function(item) {
-            var dataRow = [];
-
-            Object.keys(item).forEach(function(key) {
-                var rowData = item[key].toString();
-                var truncatedData = rowData.substring(0, 15);
-                dataRow.push(`<td>${truncatedData}</td>`);
-            });
-        tableRows.push(`<tr>${dataRow}</tr>`);
-        });
-
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
-    });
+function popOut() {
+    const time = Math.random() * 1300 + 400;
+    const hole = pickRandomHole(holes);
+    hole.classList.add("up");
+    setTimeout(function(){
+        hole.classList.remove("up");
+        if (!timeUp) popOut();
+    }, time);
 }
 
 
+function startGame(){
+    countdown = timeLimit/1000;
+    scoreBoard.textContent = 0;
+    scoreBoard.getElementsByClassName.display = "block";
+    countdownBoard.textContent = countdown;
+    timeUp = false;
+    score = 0;
+    popOut();
+    setTimeout(function(){
+        timeUp = true;
+    }, timeLimit)
+
+let startCountdown = setInterval(function(){
+    countdown -= 1;
+    countdownBoard.textContent = countdown;
+    if (countdown < 0) {
+        countdown = 0;
+        clearInterval(startCountdown);
+        countdownBoard.textContent = "holy dogs"
+    }
+}, 1000);
+}
+
+startButton.addEventListener("click", startGame);
+
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("3000.jpg")';
+}
+
+moles.forEach(mole => mole.addEventListener("click", whack));
